bd5641ca4c9499514f7ce67e22b0b47854b36e62
anhChillLe <chieu280200@gmail.com>
2024-08-23 14:11:01 +0700

    Initial Commit

---
+++ Unsplash/UnsplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashApp.swift
+//  Unsplash
+//
+//  Created by Lê Anh Chiêu on 23/8/24.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
